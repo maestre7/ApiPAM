0e657b851b3e47a9a388858808294a7162fa70a2
Alfredo Ruiz Santos <65159778+maestre7@users.noreply.github.com>
2022-02-02 13:49:14 +0100

    progress controllers

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let app = require('express')();
 
 app.use(require('express').json());
-app.use('/api', require('./routes'));
+app.use('/', require('./routes'));
 
 app.listen(3000, () => {
     console.log('Working!!!');
