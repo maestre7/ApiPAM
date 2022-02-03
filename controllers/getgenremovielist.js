21e98e2dbd3bdb91ca47524800c80cda017d5f46
Alfredo Ruiz Santos <65159778+maestre7@users.noreply.github.com>
2022-02-03 20:22:22 +0100

    data snot

--- controllers/getgenremovielist.js
+++ controllers/getgenremovielist.js
@@ -1,6 +1,15 @@
 
+const result = {
+    "genres": [
+      {
+        "id": 28,
+        "name": "Action"
+      }
+    ]
+  }
+
 function getgenremovielist(req, res){
-    res.status(200).json({ message: `Members delete id` });
+    res.status(200).json(result);
     /* res.status(401).json({ message: `Members delete id` });
     res.status(404).json({ message: `Members delete id` }); */
    }
